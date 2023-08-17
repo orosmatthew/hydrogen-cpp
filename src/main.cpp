b1c71deb8650a94127a6c0621832568dff63d738
luxluth <delphin.blehoussi93@gmail.com>
2023-08-17 22:04:48 +0200

    Some littles changes + simplify the 'tokenize()' function

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <cctype>
+#include <cstddef>
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -17,60 +20,156 @@ struct Token
     std::optional<std::string> value{};
 };
 
-std::vector<Token> tokenize(const std::string& str)
+const std::string KEYWORDS[] = {
+    "return"
+};
+
+std::string make_tokenword(std::string str)
+{
+    std::string tokenword;
+    for (char c : str)
+    {
+        if (!std::isspace(c))
+        {
+            tokenword += c;
+        }
+        else { break; }
+    }
+    return tokenword;
+}
+
+std::string make_tokenint(std::string str)
+{
+    std::string tokenint;
+    for (char c : str)
+    {
+        if (std::isdigit(c))
+        {
+            tokenint += c;
+        }
+        else { break; }
+    }
+    return tokenint;
+}
+
+std::string caret_display(size_t pos, size_t width = 1, size_t offset = 0) {
+    std::stringstream output;
+    if (width == 1) {
+        output << std::string(pos + offset + 1, ' ') << '^';
+    } else {
+        output << std::string(pos - width + offset + 1, ' ') << std::string(width, '^');
+    }
+    return output.str();
+}
+
+std::string caret_to_line(const std::string &str, size_t pos, size_t line = 1, size_t width = 1) {
+    std::stringstream output;
+    size_t line_start = 0;
+    for (size_t i = 0; i < pos; i++) {
+        if (str.at(i) == '\n') {
+            line++;
+            line_start = i + 1;
+        }
+    }
+    std::string line_str = std::to_string(line) + ": ";
+    output << line_str << str.substr(line_start, str.find('\n', line_start) - line_start)
+           << std::endl;
+    output << caret_display(pos - line_start - 1, width, line_str.size());
+    return output.str();
+}
+
+bool is_keyword(const std::string& str)
+{
+    for (const std::string& keyword : KEYWORDS)
+    {
+        if (str == keyword)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+Token get_keyword_token(const std::string& str)
+{
+    if (str == "return")
+    {
+        return Token{TokenType::_return};
+    }
+
+    std::cerr << "INTERNAL ERROR: get_keyword_token() called with invalid keyword '" << str << "'" << std::endl;
+    exit(EXIT_FAILURE);
+}
+
+std::vector<Token> tokenize(const std::string& str, const char* filename)
 {
     std::vector<Token> tokens;
-
     std::string buf;
-    for (int i = 0; i < str.length(); i++)
-    {
-        char c = str.at(i);
-        if (std::isalpha(c))
+    size_t pos = 0;
+    size_t line = 1;
+    while (pos < str.size()) {
+        if (std::isspace(str.at(pos)))
         {
-            buf.push_back(c);
-            i++;
-            while (std::isalnum(str.at(i)))
+            pos++;
+            continue;
+        }
+        else if (std::isalpha(str.at(pos)))
+        {
+            buf = make_tokenword(str.substr(pos));
+            pos += buf.size();
+            if (is_keyword(buf))
             {
-                buf.push_back(str.at(i));
-                i++;
-            }
-            i--;
-            if (buf == "return")
+                tokens.push_back(get_keyword_token(buf));
+            } else 
             {
-                tokens.push_back({.type = TokenType::_return});
-                buf.clear();
-                continue;
-            } else
-            {
-                std::cerr << "You messed up!" << std::endl;
+                std::cerr << filename << ":" << line << ":" << pos - buf.size() << ": ERROR: unexpected identifier '" << buf << "'" << std::endl;
+                std::cerr << caret_to_line(str, pos, line, buf.size()) << std::endl;
                 exit(EXIT_FAILURE);
             }
-        } else if (std::isdigit(c))
+        }
+        else if (std::isdigit(str.at(pos)))
         {
-            buf.push_back(c);
-            i++;
-            while (std::isdigit(str.at(i)))
-            {
-                buf.push_back(str.at(i));
-                i++;
-            }
-            i--;
-            tokens.push_back({.type = TokenType::int_lit, .value = buf});
-            buf.clear();
-        } else if (c == ';')
+            buf = make_tokenint(str.substr(pos));
+            pos += buf.size();
+            tokens.push_back(Token{TokenType::int_lit, buf});
+        }
+        else if (str.at(pos) == ';')
         {
-            tokens.push_back({.type = TokenType::semi});
-        } else if (std::isspace(c))
+            tokens.push_back(Token{TokenType::semi});
+            pos++;
+        } 
+        else if (str.at(pos) == '\n')
         {
-            continue;
-        } else
+            line++;
+            pos++;
+        }
+        else
         {
-            std::cerr << "You messed up!" << std::endl;
+            std::cerr << filename << ":" << line << ":" << pos + 1 << ": ERROR: unexpected character '" << str.at(pos) << "'" << std::endl;
+            std::cerr << caret_to_line(str, pos, line, 1) << std::endl;
             exit(EXIT_FAILURE);
         }
     }
 
     return tokens;
+}
+
+std::string display_token(const Token& token)
+{
+    std::stringstream output;
+    switch (token.type)
+    {
+        case TokenType::_return:
+            output << "return";
+            break;
+        case TokenType::int_lit:
+            output << "int_lit(" << token.value.value() << ")";
+            break;
+        case TokenType::semi:
+            output << "semi";
+            break;
+    }
+    return output.str();
 }
 
 std::string tokens_to_asm(const std::vector<Token>& tokens)
@@ -113,7 +212,12 @@ int main(int argc, char* argv[])
         contents = contents_stream.str();
     }
 
-    std::vector<Token> tokens = tokenize(contents);
+    std::vector<Token> tokens = tokenize(contents, argv[1]);
+    // for (const Token& token : tokens)
+    // {
+    //     std::cout << display_token(token) << std::endl;
+    // }
+
     {
         std::fstream file("out.asm", std::ios::out);
         file << tokens_to_asm(tokens);
