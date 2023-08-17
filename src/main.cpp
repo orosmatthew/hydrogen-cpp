0161fc5cc383d3c63eb37a2dcee730dd59157cdb
orosmatthew <orosmatthew@pm.me>
2023-08-17 19:06:43 -0400

    Formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,14 @@
-#include <iostream>
 #include <fstream>
-#include <sstream>
+#include <iostream>
 #include <optional>
+#include <sstream>
 #include <vector>
 
-enum class TokenType
-{
-    _return,
-    int_lit,
-    semi
-};
+enum class TokenType { _return, int_lit, semi };
 
-struct Token
-{
+struct Token {
     TokenType type;
-    std::optional<std::string> value{};
+    std::optional<std::string> value {};
 };
 
 std::vector<Token> tokenize(const std::string& str)
@@ -22,49 +16,44 @@ std::vector<Token> tokenize(const std::string& str)
     std::vector<Token> tokens;
 
     std::string buf;
-    for (int i = 0; i < str.length(); i++)
-    {
+    for (int i = 0; i < str.length(); i++) {
         char c = str.at(i);
-        if (std::isalpha(c))
-        {
+        if (std::isalpha(c)) {
             buf.push_back(c);
             i++;
-            while (std::isalnum(str.at(i)))
-            {
+            while (std::isalnum(str.at(i))) {
                 buf.push_back(str.at(i));
                 i++;
             }
             i--;
-            if (buf == "return")
-            {
-                tokens.push_back({.type = TokenType::_return});
+            if (buf == "return") {
+                tokens.push_back({ .type = TokenType::_return });
                 buf.clear();
                 continue;
-            } else
-            {
+            }
+            else {
                 std::cerr << "You messed up!" << std::endl;
                 exit(EXIT_FAILURE);
             }
-        } else if (std::isdigit(c))
-        {
+        }
+        else if (std::isdigit(c)) {
             buf.push_back(c);
             i++;
-            while (std::isdigit(str.at(i)))
-            {
+            while (std::isdigit(str.at(i))) {
                 buf.push_back(str.at(i));
                 i++;
             }
             i--;
-            tokens.push_back({.type = TokenType::int_lit, .value = buf});
+            tokens.push_back({ .type = TokenType::int_lit, .value = buf });
             buf.clear();
-        } else if (c == ';')
-        {
-            tokens.push_back({.type = TokenType::semi});
-        } else if (std::isspace(c))
-        {
+        }
+        else if (c == ';') {
+            tokens.push_back({ .type = TokenType::semi });
+        }
+        else if (std::isspace(c)) {
             continue;
-        } else
-        {
+        }
+        else {
             std::cerr << "You messed up!" << std::endl;
             exit(EXIT_FAILURE);
         }
@@ -77,15 +66,11 @@ std::string tokens_to_asm(const std::vector<Token>& tokens)
 {
     std::stringstream output;
     output << "global _start\n_start:\n";
-    for (int i = 0; i < tokens.size(); i++)
-    {
+    for (int i = 0; i < tokens.size(); i++) {
         const Token& token = tokens.at(i);
-        if (token.type == TokenType::_return)
-        {
-            if (i + 1 < tokens.size() && tokens.at(i + 1).type == TokenType::int_lit)
-            {
-                if (i + 2 < tokens.size() && tokens.at(i + 2).type == TokenType::semi)
-                {
+        if (token.type == TokenType::_return) {
+            if (i + 1 < tokens.size() && tokens.at(i + 1).type == TokenType::int_lit) {
+                if (i + 2 < tokens.size() && tokens.at(i + 2).type == TokenType::semi) {
                     output << "    mov rax, 60\n";
                     output << "    mov rdi, " << tokens.at(i + 1).value.value() << "\n";
                     output << "    syscall";
@@ -98,8 +83,7 @@ std::string tokens_to_asm(const std::vector<Token>& tokens)
 
 int main(int argc, char* argv[])
 {
-    if (argc != 2)
-    {
+    if (argc != 2) {
         std::cerr << "Incorrect usage. Correct usage is..." << std::endl;
         std::cerr << "hydro <input.hy>" << std::endl;
         return EXIT_FAILURE;
