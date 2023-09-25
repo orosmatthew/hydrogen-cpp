ee93cbeff2dbe6905c3f452d73aa080fc083ad2f
ElPettego <mattiapapaccioli@gmail.com>
2023-09-25 12:25:42 +0200

    minor optimization for error

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,16 @@
 
 #include "./generation.hpp"
 
+void error(char* msg) 
+{
+    std::cerr << msg <<std::endl;
+    exit(EXIT_FAILURE);
+}
+
 int main(int argc, char* argv[])
 {
     if (argc != 2) {
-        std::cerr << "Incorrect usage. Correct usage is..." << std::endl;
-        std::cerr << "hydro <input.hy>" << std::endl;
-        return EXIT_FAILURE;
+        error(const_cast<char*>("Incorrect usage. Correct usage is...\nhydro <input.hy>"));
     }
 
     std::string contents;
@@ -29,8 +33,7 @@ int main(int argc, char* argv[])
     std::optional<NodeProg> prog = parser.parse_prog();
 
     if (!prog.has_value()) {
-        std::cerr << "Invalid program" << std::endl;
-        exit(EXIT_FAILURE);
+        error(const_cast<char*>("Invalid program"));
     }
 
     Generator generator(prog.value());
